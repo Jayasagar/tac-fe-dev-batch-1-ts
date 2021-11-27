c9b9f31f52089c341ef2e8349b511525be898b32
swathidevaraplli <swathi.d118@gmail.com>
2021-11-27 16:26:18 +0530

    Added the members of the class Cycle and also created object for the class Cycle.

--- CycleRentalSystem/Classes.ts
+++ CycleRentalSystem/Classes.ts
@@ -1,5 +1,12 @@
 class Cycle {
-    
+    rentPerHour: number;
+    models: Model[];
+    print() {
+        console.log("Rent Per Hous:", this.rentPerHour);
+        this.models.forEach(model => {
+            model.print();
+        });
+    }
 }
 class Model {
 
@@ -7,5 +14,6 @@ class Model {
 class Order {
     
 }
+var gear = new Cycle();
 
 
