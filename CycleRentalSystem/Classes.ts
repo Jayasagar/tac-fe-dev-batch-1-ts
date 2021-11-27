39f788766cf4fd87e1cf9bc307c1a14fbf630350
swathidevaraplli <swathi.d118@gmail.com>
2021-11-27 18:15:54 +0530

    Using setData method I was assigned values to the parameters and added some models by calling setData method. Anyone of you can add the method addModel method.

--- CycleRentalSystem/Classes.ts
+++ CycleRentalSystem/Classes.ts
@@ -1,6 +1,12 @@
 class Cycle {
+    companyName: string;
     rentPerHour: number;
     models: Model[];
+    setData(cName:string, rentPHour: number, []): void {
+        this.companyName = cName;
+        this.rentPerHour = rentPHour;
+        this.models = [];
+    }
     print() {
         console.log("Rent Per Hours:", this.rentPerHour);
         this.models.forEach(model => {
@@ -25,6 +31,15 @@ const gearCycle = new Model(5);
 //gearCycle.print();
 const weightLess = new Model(3);
 //weighLess.print(); 
-var gear = new Cycle();
-
-
+const tubeLess = new Model(3);
+var Hero = new Cycle();
+Hero.setData("Hero", 5, [gearCycle]);
+Hero.addModel(gearCycle);
+Hero.print();
+var Atlas = new Cycle();
+Atlas.setData("Atlas", 6, [weightLess]);
+Atlas.addModel(weightLess);
+Atlas.print();
+var Hercules = new Cycle();
+Hercules.addModel([tubeLess]);
+Hercules.print();
