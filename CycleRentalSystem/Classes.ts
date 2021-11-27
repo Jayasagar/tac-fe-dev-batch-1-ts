243ebcea05d7672f978358e82fcb074c580a9c37
oniranjna <oruguntaniranjan@gmail.com>
2021-11-27 16:56:13 +0530

    Added the members of class Model and also created objects for the Model class

--- CycleRentalSystem/Classes.ts
+++ CycleRentalSystem/Classes.ts
@@ -2,18 +2,29 @@ class Cycle {
     rentPerHour: number;
     models: Model[];
     print() {
-        console.log("Rent Per Hous:", this.rentPerHour);
+        console.log("Rent Per Hours:", this.rentPerHour);
         this.models.forEach(model => {
             model.print();
         });
     }
 }
 class Model {
+    rentPerHour: number;
+    constructor(rentPerHour: number) {
+        this.rentPerHour = rentPerHour;
+    }
+    print() {
+        console.log("Rent Per Hours:", this.rentPerHour);
+    }
 
 }
 class Order {
     
 }
+const gearCycle = new Model(5);
+//gearCycle.print();
+const weightLess = new Model(3);
+//weighLess.print(); 
 var gear = new Cycle();
 
 
