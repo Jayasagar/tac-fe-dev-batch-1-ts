6c799853984e331e4de8fb15219a59e64b6ca13c
SwathiKrishna27 <swathikrishna2627@gmail.com>
2021-11-29 02:51:49 +0530

    Subcription plans

--- CycleRentalSystem/Subscription.ts
+++ CycleRentalSystem/Subscription.ts
@@ -1,4 +1,20 @@
-import './Classes.ts'
-class Subcription {
-
+import {Model} from './ModelClass'
+class Subcription extends Model {
+    plan: String;
+    amountPerplan: Number;
+    constructor (plan: String, amountperPplan: Number) {
+        super(3);
+        this.plan = plan;
+        this.amountPerplan = amountPerplan;
+    }
+    print() {
+        console.log("Subcsription for.", this.plan, "Rs.", this.amountPerplan);
+    }
 }
+
+var plan1 = new Subcription('weekly', 50);
+plan1.print();
+var plan2 = new Subcription('monthly', 300);
+plan2.print();
+var plan3 = new Subcription('quaterly', 1000);
+plan3.print();   
