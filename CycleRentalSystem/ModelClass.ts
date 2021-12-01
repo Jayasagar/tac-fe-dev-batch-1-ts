676dcd022569d064b3d1b39a1d8d33c23bdca298
oniranjna <oruguntaniranjan@gmail.com>
2021-12-01 15:20:48 +0530

    Models

--- CycleRentalSystem/ModelClass.ts
+++ CycleRentalSystem/ModelClass.ts
@@ -1,11 +1,17 @@
 export class Model {
-    model: String;
-    constructor (model: String) {
-        this.model = model;
+    name: string;
+    rentPerHour: number;
+    constructor(name: string, rentPerHour: number) {
+        this.name = name;
+        this.rentPerHour = rentPerHour;
     }
     print() {
-        console.log("model name:", this.model);
+        console.log("Model Name:", this.name, ", Rent Per Hours:", this.rentPerHour);
     }
 }
-const gearCycle = new Model('gearCycle');
-gearCycle.print();
+const gearCycle = new Model("gearCycle", 5);
+//gearCycle.print();
+const weightLess = new Model("weightLess", 6);
+//weighLess.print();
+const tubeLess = new Model("tubeLess", 3);
+//tubeLess.print();
